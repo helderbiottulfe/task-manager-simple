0388b10472cddf92febca3046932962dcc8577de
helderbiottulfe <helder.biottulfe@hotmail.com>
2026-02-06 03:29:48 +0000

    Added alert notification for task completion

--- app/script.js
+++ app/script.js
@@ -1,2 +1,5 @@
 // script.js
 // Notification logic will be added later
+document.getElementById("completeBtn").addEventListener("click", function() {
+    alert("Task completed! Notification sent.");
+});
